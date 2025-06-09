a18b2e49a9c16f73d3812390f61be37d9fb35d04
DSFKnight <Gran122004@gmail.com>
2025-06-09 16:56:31 +0300

    ver 1

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -1,15 +1,15 @@
-import { useSelector } from '../../services/store';
+import { FC } from 'react';
+import { useSelector } from '@app-store';
 
 import styles from './constructor-page.module.css';
 
-import { BurgerIngredients } from '../../components';
-import { BurgerConstructor } from '../../components';
-import { Preloader } from '../../components/ui';
-import { FC } from 'react';
+import { BurgerIngredients } from '@components';
+import { BurgerConstructor } from '@components';
+import { Preloader } from '@ui';
+import { ingredientsLoadingSelector } from '../../services/slices/ingredientsSlice';
 
 export const ConstructorPage: FC = () => {
-  /** TODO: взять переменную из стора */
-  const isIngredientsLoading = false;
+  const isIngredientsLoading = useSelector(ingredientsLoadingSelector);
 
   return (
     <>
